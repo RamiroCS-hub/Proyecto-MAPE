07e446e21bae5588292baa787f915c2ecb819203
Ramadan3316 <104595014+RamiroCS-hub@users.noreply.github.com>
2022-10-01 18:24:44 -0300

    Conexion firebase

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 firebase = firebase.FirebaseApplication("https://fir-test-50790-default-rtdb.firebaseio.com/",None)
 
 resultado = firebase.get("\Alarma", None)
-print(resultado)
+
 if resultado == None:
     now = datetime.now()	
     time = now.strftime("%B %d %Y %H:%M:%S")
