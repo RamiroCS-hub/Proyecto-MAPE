c405e6191e4cf8f3641af48678253dbe9b2c3025
Ramadan3316 <104595014+RamiroCS-hub@users.noreply.github.com>
2022-10-02 19:39:44 -0300

    Arduino mandar email

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,6 @@
-//#include <Arduino.h>
+#include <Arduino.h>
 #include <WiFi.h>
-#include <ESP_Mail_Client.h>
-
-
+#include <ESP_Mail_Client.h> //Documentación: https://github.com/mobizt/ESP-Mail-Client/blob/master/README.md
 
 //Configuracion del servidor NTP
 const char* ntpServer = "pool.ntp.org"; //Servidor NTP
@@ -18,7 +16,6 @@ const int daylightOffset_sec = 0;       //Compensa
 #define SMTP_PORT esp_mail_smtp_port_465
 
 // Credenciales de la cuenta remitente
-
 #define AUTHOR_EMAIL "pythonnexample@gmail.com"
 #define AUTHOR_PASSWORD "rjvjltqrzdatisvu"
 
@@ -26,62 +23,64 @@ const int daylightOffset_sec = 0;       //Compensa
 SMTPSession smtp;
 ESP32Time rtc;
 
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println();
-  Serial.print("Conectando al Acess point...");
+void setup(){
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+    Serial.begin(9600);
+    Serial.println();
+    Serial.print("Conectando al Acess point...");
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
-  }
-configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    }
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  Serial.println("WiFi connectado.");
-  Serial.println("Direccion IP: ");
- 
-  smtp.debug(1);
-
-  smtp.callback(smtpCallback);
-
-  /* Declaramos una Sesion para poder configurar*/
-  ESP_Mail_Session session;
-
-  session.server.host_name = SMTP_HOST;
-  session.server.port = SMTP_PORT;
-  session.login.email = AUTHOR_EMAIL;
-  session.login.password = AUTHOR_PASSWORD;
-
-  /* Declaramos una variable para el mensaje */
-  SMTP_Message message;
-
-  /* Especificar los encabezados*/
-  message.sender.email = AUTHOR_EMAIL;
-  message.subject = "ALARMA ACTIVADA";
-
-  String textMsg = rtc.getTime("%A, %B %d %Y %H:%M:%S");
-  message.text.content = textMsg.c_str();
-
-  message.text.charSet = "us-ascii";
-  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit; // sin codificación
-
-  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
-
-  /* Conectar al servidor con la configuracios de la sesión*/
-  if (!smtp.connect(&session))
-    return;
-
-  /* Envía el correo y cierra la sesión */
-  if (!MailClient.sendMail(&smtp, &message))
-    Serial.println("Error sending Email, " + smtp.errorReason());
-
-  ESP_MAIL_PRINTF("Liberar memoria: %d\n", MailClient.getFreeHeap());
-  
+}
+void sendEmail (string);
+void loop(){
+    string email;
+    Firebase.getInt(fbdo,"alarma");
+    if(firebaseData.intData() == 1){
+        Firebase.getString(fbdo,"email")
+        email = firebaseData.stringData()
+        sendEmail(email);
+    }
 }
 
-void loop(){
+void sendEmail (email){
     
+    strin textMsg = rtc.getTime("%A, %B %d %Y %H:%M:%S");
+
+    /* Declaramos una Sesion para poder configurar*/
+    ESP_Mail_Session session;
+
+    session.server.host_name = SMTP_HOST;
+    session.server.port = SMTP_PORT;
+    session.login.email = AUTHOR_EMAIL;
+    session.login.password = AUTHOR_PASSWORD;
+
+    /* Declaramos una variable para el mensaje */
+    SMTP_Message message;
+
+    /* Especificar los encabezados*/
+    message.sender.email = AUTHOR_EMAIL;
+    message.subject = "ALARMA ACTIVADA";
+    message.addRecipient(email,email)
+    
+    message.text.content = textMsg.c_str();
+
+    message.text.charSet = "us-ascii";
+    message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit; // sin codificación
+
+    message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_normal;
+
+    /* Conectar al servidor con la configuracios de la sesión*/
+    if (!smtp.connect(&session)) Serial.pintln("No se pudo conectar")
+    
+    /* Envía el correo y cierra la sesión */
+    if (!MailClient.sendMail(&smtp, &message)){
+        Serial.println("Error sending Email, " + smtp.errorReason());
+    }
+    ESP_MAIL_PRINTF("Liberar memoria: %d\n", MailClient.getFreeHeap());
 }
